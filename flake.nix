10a5eb2a3591e324826f5f77dc79107caf4f3244
Jon Hermansen <jon@jh86.org>
2025-07-17 18:29:08 -0400

    testing

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
-  description = "A very basic flake";
-
+  description = "Jon test";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
   };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages.x86_64-linux.emacs = nixpkgs.legacyPackages.x86_64-linux.emacs;
+      packages.x86_64-linux.zsh = nixpkgs.legacyPackages.x86_64-linux.zsh;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.zsh;
+    };
 }
